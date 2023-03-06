c991b05c6bb3edd4e53b677965a4391117a70c19
kofancy2023 <kofancy@qq.com>
2023-03-06 19:15:35 +0800

    kofancy:初始化代码框架

---
+++ chatbot-api-interface/src/main/java/com/kofancy/ai/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.kofancy.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
+
---
+++ chatbot-api-interface/src/test/java/com/kofancy/ai/test/ApiTest.java
@@ -0,0 +1,7 @@
+package com.kofancy.ai.test;
+
+/**
+ * 单元测试
+ */
+public class ApiTest {
+}
